89ec54613778dc4721ea1fee4c1710bc21a47c28
Richard Layton <rlayton@gmail.com>
2021-06-22 02:04:42 -0600

    .

--- plotter_daemon/clean.sh
+++ plotter_daemon/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 find /chia/dst/* -type 'f' -size -100000M -print -delete
-find /chia/tmp/* -type 'f' -size -100000M -print -delete
+find /chia/tmp/* -type 'f' -print -delete
 find /chia/plotters/* -type 'f' -size -100000M -print -delete
