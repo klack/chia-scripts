406d4fed33b91ce6667f96ea5bab48a3059512d0
Richard Layton <rlayton@gmail.com>
2021-06-22 21:15:52 -0600

    Fix

--- update.sh
+++ update.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -x
 git fetch origin master
 git reset --hard origin/master
+sudo chmod +x /opt/chia-scripts/*.sh && \
+sudo chmod +x /opt/chia-scripts/farm/*.sh
+sudo chmod +x /opt/chia-scripts/plot/*.sh
